811ab5ee9df0fb871597b86cdc527f7454287572
rabiya <Rabiya>
2026-05-22 08:18:43 +0300

    Fix flake8 formatting issues

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,13 +1,11 @@
-import sys
 import os
+import sys
 
 sys.path.append(
     os.path.abspath(
         os.path.join(os.path.dirname(__file__), "..")
     )
 )
-
-from src.data_loader import load_data
 
 
 def test_placeholder():
